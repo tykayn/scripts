0ed6e08ab17df530cb5e99402f7d0a99bd28f3cb
Tykayn <contact@cipherbliss.com>
2022-10-20 11:01:13 +0200

    add crontab, update git from several forges

--- update_git_projects.sh
+++ update_git_projects.sh
@@ -2,19 +2,23 @@
 # update all projects
 # list of framagit repos to clone
 
-declare -a StringArray=("caisse-bliss" "duniter" "joinfediverse" "date-poll-api" "mastodon" "peertube" "events-liberator" "gitall" "dotclear-importer" "mobilizon" "fanzine-log" "crossed-words" "generator-tk" "circles" "card-deck" "sf-probe" "mastermind" "portfolio" "time-tracker" "cipherbliss" "caisse-bliss-frontend" "compta" "trafficjam" "ical-generator" "blueprint-cipherbliss" "dotclear2wordpress" "api" "diaspora" )
+declare -a StringArrayFramagit=("caisse-bliss" "joinfediverse" "date-poll-api" "mastodon" "peertube" "events-liberator" "gitall" "dotclear-importer" "mobilizon" "fanzine-log" "crossed-words" "generator-tk" "circles" "card-deck" "sf-probe" "mastermind" "portfolio" "time-tracker" "cipherbliss" "caisse-bliss-frontend" "compta" "trafficjam" "ical-generator" "blueprint-cipherbliss" "dotclear2wordpress" "api" "diaspora" )
+
+declare -a StringArrayForgeChapril=("transcription" "org-report-stats" "multi-account-post-schedule-mastodon" "framalibre-scraping" "scripts" "melting-pot" "funky-framadate-front" "rss-feeder-mobilizon" "mastodon-archive-stats" "gtg2json" "libreavous-audio-reader" "osm_my_commerce" "fromage-js" "ueberauth_openstreetmap" "events-liberator")
 
 prefix_framagit='https://framagit.org/tykayn/'
-username="${SUDO_USER:-${USER}}"
-cloning_place="/home/$username/www/"
+prefix_forgechapril='https://forge.chapril.org/tykayn/'
 
-echo $cloning_place
-#mkdir $cloning_place
+cloning_place="/home/$USER/www/"
+
+mkdir -p $cloning_place
+
 cd $cloning_place
-git config --global credential.helper store
 
 echo "Number of items in original list: ${#list[*]}"
-for folder_name in ${StringArray[@]}
+echo "cloning from framagit"
+
+for folder_name in ${StringArrayFramagit[@]}
 do
   echo "check project ${folder_name}"
 	if [ ! -d $folder_name ]
@@ -31,22 +35,27 @@ do
 
 		git remote -v
 		git fetch origin
-		 # update dependencies if there is an update script
-    if [ -f "update.sh" ]
-	    then
-	    echo "update.sh exists"
-#	      bash update.sh
-	  fi
+	fi
+done
+# done framagit
 
-	  if [ -f "yarn.lock" ]
-	    then
-	    echo "installing yarn packages"
-#	      yarn install --pure-lockfile
-#	  elif [ -f "package.lock" ]
-#	    npm i
-	  fi
+for folder_name in ${StringArrayForgeChapril[@]}
+do
+  echo "check project ${folder_name}"
+	if [ ! -d $folder_name ]
+	then
+		echo "cloning ${folder_name}"
+		git clone "${prefix_forgechapril}${folder_name}.git"
 
-		cd ..
+	# test existence of a folder
+	# if there is no folder, clone it
+	# else, update with fetch from origin
+	else
+		echo "##### update project $folder_name"
+		cd $folder_name
+
+		git remote -v
+		git fetch origin
 	fi
 done
 
